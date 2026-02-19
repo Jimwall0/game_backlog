67ec73cce28da80540d27423b45193288e0a5c0d
Tsimmuaj Yang <tyang4204@gmail.com>
2026-02-19 15:47:36 +0000

    sqlite install

---
+++ app/(tabs)/listview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function listview() {
+    return (
+        <View>
+            <Text>
+                This is where the list of games go
+            </Text>
+        </View>
+    )
+}
