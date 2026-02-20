77bd1321b1243bc1d424df9d9a560cddf04d996f
Tsimmuaj Yang <tyang4204@gmail.com>
2026-02-20 00:41:50 +0000

    running

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+/** @type {import('expo/metro-config').MetroConfig} */
 const { getDefaultConfig } = require('expo/metro-config');
-
+const config = getDefaultConfig(__dirname);
+ 
+// Add wasm asset support
 config.resolver.assetExts.push('wasm');
 
+// Add COEP and COOP headers to support SharedArrayBuffer
 config.server.enhanceMiddleware = (middleware) => {
-    return (req, res, next) => {
-        res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-        middleware(req, res, next);
-    };
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    middleware(req, res, next);
+  };
 };
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
+ 
 module.exports = config;
