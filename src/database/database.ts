7affc64af96022c7a594cea449fe0fbae4c3e9d3
Tsimmuaj Yang <tyang4204@gmail.com>
2026-02-20 03:05:11 +0000

    rm all that sync garbage

--- src/database/database.ts
+++ src/database/database.ts
@@ -7,57 +7,15 @@ type Game = {
   reason?: string;
 }
 
-const db: SQLite.SQLiteDatabase = SQLite.openDatabaseSync('app.db');
+const db: SQLite.SQLiteDatabase = await SQLite.openDatabaseAsync('app.db');
 
-export const initDB = (): void => {
-  db.execSync(`
-    CREATE TABLE IF NOT EXISTS games (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      studio TEXT NOT NULL,
-      reason TEXT
-    );
-  `);
-
-  const count = db.getFirstSync<{ count: number}>(`SELECT COUNT(*) as count FROM games;`);
-
-  if (count?.count === 0) {
-    db.runSync(
-      `INSERT INTO games (title, studio, reason) VALUES (?, ?, ?);`,
-      ['Mincraft', 'Mojang', "Don't own it for some reason"]
-    );
-  }
-};
-
-// 🔹 CREATE
-export const insertGame = (title: string, studio: string, reason?: string): void => {
-  db.runSync(
-    `INSERT INTO games (title, studio, reason) VALUES (?, ?, ?);`,
-    [title, studio, reason ?? null]
+await db.execAsync(`
+  CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    studio TEXT NOT NULL,
+    reason TEXT
   );
-};
-
-
-// 🔹 READ
-export const getAllGames = (): Game[] => {
-  return db.getAllSync(`SELECT * FROM games;`);
-};
-
-// 🔹 UPDATE
-export const updateGame = (id: number, title: string, studio: string, reason?:string): void => {
-  db.runSync(
-    `UPDATE games SET title = ?, studio = ?, reason = ? WHERE id = ?;`,
-    [title, studio, reason ?? null, id]
-  );
-};
-
-
-// 🔹 DELETE
-export const deleteGame = (id: number) => {
-  return db.runSync(
-    `DELETE FROM games WHERE id = ?;`,
-    [id]
-  );
-};
+`);
 
 export default db;
