0d45854e6fb9fbf827b27b752fe8cdcf8157fa9b
Tsimmuaj Yang <tyang4204@gmail.com>
2026-02-20 00:23:04 +0000

    save

--- app/(tabs)/listview.tsx
+++ app/(tabs)/listview.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
-
 function listview() {
     return (
         <View>
+            <SQLiteProvider>
             <Text>
                 This is where the list of games go
             </Text>
+            </SQLiteProvider>
         </View>
     )
 }
--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 config.resolver.assetExts.push('wasm');
 
+config.server.enhanceMiddleware = (middleware) => {
+    return (req, res, next) => {
+        res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        middleware(req, res, next);
+    };
+};
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
